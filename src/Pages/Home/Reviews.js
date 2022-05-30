d96a645519a1d02bc08cd1aa19f78e4bbf48afe9
MohammadZakariaYunus <mohammadzakariaabdullahyunus99@gmail.com>
2022-05-30 12:07:18 +0600

    reviews done

--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -36,13 +36,14 @@ const Reviews = () => {
         <div className='mx-12' >
             <p>Customer Reviews</p>
             <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
+
                 {
                     reviews.map(review => <Review
                         key={review._id}
                         review={review}
                     ></Review>)
-
                 }
+
             </div>
         </div >
     );
